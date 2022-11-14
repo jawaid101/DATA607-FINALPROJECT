e69fe87632bafa435b419da6bf01257641945353
Jawaid Hakim <himalayahall@gmail.com>
2022-11-14 14:25:04 -0500

    Refactor
    Signed-off-by: Jawaid Hakim <himalayahall@gmail.com>

---
+++ src/FilePaths.R
@@ -0,0 +1,57 @@
+source('./Consts.R')
+
+#
+# Get full path to href file
+# 
+# Parameters:
+#   file_type: c('href', 'ts')
+#   global: TRUE for Global data file, FALSE for USA data file
+Output.GetHrefFilePath <- function(file_type = 'href', global = FALSE) {
+    file_name <- NULL
+    if (str_to_lower(file_type) == 'href') {
+        if (global) {
+            file_name <- DB.HREF_FILE_GLOBAL
+        }
+        else {
+            file_name <- DB.HREF_FILE_USA
+        }
+    }
+    else if (str_to_lower(file_type) == 'ts')
+        # timestamp
+    {
+        if (global) {
+            file_name <- DB.TS_FILE_GLOBAL
+        }
+        else {
+            file_name <- DB.TS_FILE_USA
+        }
+    }
+    else {
+        print("Error: invalid file type")
+        return (NA)
+    }
+    return (paste0(DB.SUMMARY_PATH, file_name, ".csv"))
+}
+
+#
+# Get full path to data file
+# 
+# Parameters:
+#   targetDate: MM-DD-YYYY
+#   global: TRUE for Global data file, FALSE for USA data file
+Output.GetDataFilePath <- function(targetDate, global = FALSE) {
+    file_name <- NULL
+    
+    target_year <- lubridate::year(targetDate)
+    target_month <- toupper(lubridate::month(targetDate, label = TRUE, abbr = TRUE))
+    
+    if (global) {
+        file_name <- paste0('global-', targetDate)
+    }
+    else {
+        file_name <- paste0('usa-', targetDate)
+    }
+    
+    return (paste0(DB.PATH, target_year, "/", target_month, "/", file_name, ".csv"))
+}
+
