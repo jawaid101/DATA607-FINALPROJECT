e997e51a8fb4c248fbf93182beb95301e360c330
Jawaid Hakim <himalayahall@gmail.com>
2022-11-15 08:49:08 -0500

    renaming
    Signed-off-by: Jawaid Hakim <himalayahall@gmail.com>

--- src/Consts.R
+++ src/Consts.R
@@ -4,13 +4,11 @@ library(lubridate)
 DB.PATH <- "../data/processed/"
 DB.SUMMARY_PATH <- '../data/processed/summary/'
 
-#DB.DATA_FILE_USA <- "covid_data_usa"
-DB.HREF_FILE_USA <- "covid_href_usa"
-DB.TS_FILE_USA <- "covid_ts_usa"
+DB.HREF_FILE_USA <- "usa_href"
+DB.TS_FILE_USA <- "usa_ts"
 
-#DB.DATA_FILE_GLOBAL <- "covid_data_glocal"
-DB.HREF_FILE_GLOBAL <- "covid_href_global"
-DB.TS_FILE_GLOBAL <- "covid_ts_global"
+DB.HREF_FILE_GLOBAL <- "global_href"
+DB.TS_FILE_GLOBAL <- "global_ts"
 
 URL.USA <- "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports_us"
 URL.GLOBAL <- "https://github.com/CSSEGISandData/COVID-19/tree/master/csse_covid_19_data/csse_covid_19_daily_reports"
