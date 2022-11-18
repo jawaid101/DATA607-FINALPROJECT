b5a0f7970478947e827f56a77c5cf60119484254
Jawaid Hakim <himalayahall@gmail.com>
2022-11-18 15:11:34 -0500

    changes
    Signed-off-by: Jawaid Hakim <himalayahall@gmail.com>

--- src/Consts.R
+++ src/Consts.R
@@ -1,6 +1,8 @@
 DB.PATH <- "../data/processed/"
 DB.SUMMARY_PATH <- paste0(DB.PATH, 'summary/')
 DB.TIME_SERIES_PATH <- paste0(DB.PATH, 'timeseries/')
+DB.OUR_WORLD_IN_DATA_PATH <- paste0(DB.PATH, 'owid/')
+DB.COUNTRY_LAT_LONG_DATA_PATH <- paste0(DB.PATH, 'owid/')
 
 DB.HREF_FILE_USA <- "usa_href"
 DB.TS_FILE_USA <- "usa_ts"
@@ -19,19 +21,41 @@ URL.GLOBAL <-
 
 # Time series
 FILE.TS.US.DEATHS <- "time_series_covid19_deaths_US.csv"
-URL.TS.US.DEATHS <- 
-    paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/", FILE.TS.US.DEATHS)
+URL.TS.US.DEATHS <-
+    paste0(
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/",
+        FILE.TS.US.DEATHS
+    )
 
 FILE.TS.US.CONFIRMED <- "time_series_covid19_confirmed_US.csv"
-URL.TS.US.CONFIRMED <- 
-    paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/", FILE.TS.US.CONFIRMED)
+URL.TS.US.CONFIRMED <-
+    paste0(
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/",
+        FILE.TS.US.CONFIRMED
+    )
 
-FILE.TS.GLOBAL.CONFIRMED <- "time_series_covid19_confirmed_global.csv"
-URL.TS.GLOBAL.CONFIRMED <- 
-    paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/", FILE.TS.GLOBAL.CONFIRMED)
+FILE.TS.GLOBAL.CONFIRMED <-
+    "time_series_covid19_confirmed_global.csv"
+URL.TS.GLOBAL.CONFIRMED <-
+    paste0(
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/",
+        FILE.TS.GLOBAL.CONFIRMED
+    )
 
 FILE.TS.GLOBAL.DEATHS <- "time_series_covid19_deaths_global.csv"
-URL.TS.GLOBAL.DEATHS <- 
-    paste0("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/", FILE.TS.GLOBAL.DEATHS)
+URL.TS.GLOBAL.DEATHS <-
+    paste0(
+        "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/",
+        FILE.TS.GLOBAL.DEATHS
+    )
+
+FILE.OUR_WORLD_IN_DATA <- "owid-covid-data.csv"
+URL.OUR_WORLD_IN_DATA <-
+    paste0(
+        "https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/",
+        FILE.OUR_WORLD_IN_DATA
+    )
+
+FILE.COUNTRY_LAT_LONG <- "country_lat_long.csv"
 
 SELENIUM.PORT <- 4545L
